3ee66d1f4dd8a97ccc56459b03498b619ca87a30
Mirza Abdullah Baig <84795041+AlwaysBlue-dev@users.noreply.github.com>
2025-05-13 16:50:24 +0500

    update database settings

--- EventsHub/settings.py
+++ EventsHub/settings.py
@@ -69,11 +69,11 @@ WSGI_APPLICATION = 'EventsHub.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
-
 
 
 # Password validation
