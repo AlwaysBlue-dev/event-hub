0a897b1c27a9e133bc0cf5c9642ac276c5a74d09
Mirza Abdullah Baig <84795041+AlwaysBlue-dev@users.noreply.github.com>
2025-05-21 15:58:16 +0500

    update allowrd host

--- EventsHub/settings.py
+++ EventsHub/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', cast=bool)
 
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['web-production-1077.up.railway.app']
 
     
 # Application definition
