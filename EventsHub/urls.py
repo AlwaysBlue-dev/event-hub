9f651bc293d107d6c5d74a05c88566c20ddc7891
Mirza Abdullah Baig <84795041+AlwaysBlue-dev@users.noreply.github.com>
2025-05-21 17:21:24 +0500

    update media issue in project

--- EventsHub/urls.py
+++ EventsHub/urls.py
@@ -6,4 +6,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("MainApp.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+# # Static and media files
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
