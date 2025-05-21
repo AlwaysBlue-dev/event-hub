10145509dd8d153b0578ee5077611e1373ca61f4
Mirza Abdullah Baig <84795041+AlwaysBlue-dev@users.noreply.github.com>
2025-05-21 15:39:07 +0500

    update

--- EventsHub/settings.py
+++ EventsHub/settings.py
@@ -12,8 +12,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', cast=bool)
 
 
